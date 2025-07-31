edbadeb299cc5ed06ce2456ffb9a0a7bd55d7463
wilmarRamirez <wilmar.06@hotmail.com>
2025-07-30 20:44:13 -0500

    feat:continuar

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { RecordsController } from './controller/records.controller';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot(), AuthModule],
+  controllers: [RecordsController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
